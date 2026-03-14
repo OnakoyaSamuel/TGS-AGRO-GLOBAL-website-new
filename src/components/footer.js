0e4f2cf99adfec6d832fff4a6d97eb7c0f0b9125
OnakoyaSamuel <tgsagroglobal007@gmail.com>
2026-03-14 04:49:27 +0100

    Add Footer component with copyright information

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      © 2026 TGS AGRO GLOBAL LIMITED | All Rights Reserved
+    </footer>
+  );
+}
+
+export default Footer;
